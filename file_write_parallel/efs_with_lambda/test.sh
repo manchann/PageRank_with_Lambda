d71524fd2998dc84caeb92dbb6babd96e18b948a
manchann <chl8273@naver.com>
2021-01-14 21:30:09 +0900

    file lock 실험

--- file_write_parallel/efs_with_lambda/test.sh
+++ file_write_parallel/efs_with_lambda/test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 function_name="jg-file-write-efs"
+handler="lambda_function_lock"
 efs_path="/mnt/efs/ap/read_file"
 sudo rm -rf $efs_path
-#aws lambda update-function-configuration --function-name $function_name --memory-size 256
-#sleep 5
-#aws lambda update-function-configuration --function-name $function_name --memory-size 512
+aws lambda update-function-configuration --function-name $function_name --handler $handler.lambda_handler --memory-size 256
+sleep 5
+aws lambda update-function-configuration --function-name $function_name --memory-size 512
 sleep 2
 sudo python3 ./init.py
 sleep 2
