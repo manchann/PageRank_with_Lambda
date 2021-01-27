0a2b093569a4510b08f06c565bc0605022e61de7
manchann <chl8273@naver.com>
2021-01-27 15:56:18 +0900

    page test

--- pagerank_lambda/dynamodb/init_to_s3.py
+++ pagerank_lambda/dynamodb/init_to_s3.py
@@ -44,6 +44,7 @@ page_file = s3_client.get_object(Bucket=bucket, Key=config["pages"])
 def get_page_relation(file, pages):
     page_relations = {}
     pages = pages['Body'].read().decode()
+    print(pages)
     lines = pages.split("\n")
     print(lines)
     for line in lines:
@@ -74,7 +75,7 @@ def get_page_relation(file, pages):
 # page의 관계들이 담겨있는 파일을 가지고 dictionary 관계 데이터셋을 만듭니다.
 thread_list = []
 
-for idx in range(500):
+for idx in range(10):
     t = Thread(target=get_page_relation, args=(idx, page_file,))
     t.start()
     thread_list.append(t)
