5dedf13d81c461cbedbed4fbbac9b0a2a34d1c2a
manchann <chl8273@naver.com>
2021-02-04 19:20:37 +0900

    sequential하게 변경

--- pagerank_lambda/dynamodb/driver.py
+++ pagerank_lambda/dynamodb/driver.py
@@ -124,12 +124,14 @@ def init_iter(page):
 
 init_return = []
 for page in total_pages:
-    init_t = Thread(target=init_iter,
-                    args=(page,))
-    init_t.start()
-    init_return.append(init_t)
-for init_t in init_return:
-    init_t.join()
+    init_iter(page)
+# for page in total_pages:
+#     init_t = Thread(target=init_iter,
+#                     args=(page,))
+#     init_t.start()
+#     init_return.append(init_t)
+# for init_t in init_return:
+#     init_t.join()
 
 # 모든 page의 초기 Rank값은 1/(전체 페이지 수) 의 값을 가집니다.
 
