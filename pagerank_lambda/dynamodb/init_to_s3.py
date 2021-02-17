374ec36840e57f8a0aad83ad4e345bd575796d40
manchann <chl8273@naver.com>
2021-02-17 21:00:59 +0900

    error 처리

--- pagerank_lambda/dynamodb/init_to_s3.py
+++ pagerank_lambda/dynamodb/init_to_s3.py
@@ -56,6 +56,8 @@ def get_page_relation(file, pages):
         try:
             source = line.split("\t")[0]
             destination = line.split("\t")[1].replace("\r", "")
+            print('source', source)
+            print('des', destination)
             if source == destination:
                 continue
             key_compared = int(destination)
