f90626aad1dd3e60bc92e4d2853dcfb296e5b222
manchann <chl8273@naver.com>
2021-02-25 23:36:59 +0900

    index 수정

--- pagerank_lambda/efs/driver.py
+++ pagerank_lambda/efs/driver.py
@@ -118,8 +118,8 @@ for page in total_pages:
     with open(rank_path, 'r+b', 0) as f:
         # file lock : start_byte 부터 10개의 byte 범위를 lock
         fcntl.lockf(f, fcntl.LOCK_EX, 10, page, 1)
-        for idx in range(page, page + 10):
-            f.seek(page * idx)
+        for idx in range(10):
+            f.seek(page * idx + 1)
             f.write(pagerank_init[idx])
         # file lock : start_byte 부터 10개의 byte 범위를 unlock
         fcntl.lockf(f, fcntl.LOCK_UN, page, 1)
