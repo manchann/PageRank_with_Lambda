7fea5b5b0e522da13a6aa7b894974e6e0ce9b0a4
manchann <chl8273@naver.com>
2021-02-25 23:45:09 +0900

    encoding

--- pagerank_lambda/efs/driver.py
+++ pagerank_lambda/efs/driver.py
@@ -120,7 +120,7 @@ for page in total_pages:
         fcntl.lockf(f, fcntl.LOCK_EX, 10, page, 1)
         for idx in range(10):
             f.seek(page * idx + 1)
-            f.write(pagerank_init[idx])
+            f.write(pagerank_init[idx].encode())
         # file lock : start_byte 부터 10개의 byte 범위를 unlock
         fcntl.lockf(f, fcntl.LOCK_UN, page, 1)
         f.close()
