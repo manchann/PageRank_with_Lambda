ba6e19f8464329c676c7edfe8acd80ffe9bfa47e
manchann <chl8273@naver.com>
2021-02-25 22:07:20 +0900

    case efs

---
+++ pagerank_lambda/efs/driver.py
@@ -0,0 +1,154 @@
+import json
+import boto3
+import glob
+import os
+import subprocess
+import lambdautils
+import decimal
+import time
+from botocore.client import Config
+from boto3.dynamodb.types import DYNAMODB_CONTEXT
+from threading import Thread
+import fcntl
+
+os.system('export serverless_mapreduce_role=arn:aws:iam::741926482963:role/biglambda_role')
+
+s3 = boto3.resource('s3')
+s3_client = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+db_name = 'jg-pagerank'
+table = dynamodb.Table(db_name)
+
+config = json.loads(open('driverconfig.json', 'r').read())
+
+bucket = config["bucket"]
+region = config["region"]
+lambda_memory = config["lambdaMemory"]
+concurrent_lambdas = config["concurrentLambdas"]  # 동시 실행 가능 수
+lambda_read_timeout = config["lambda_read_timeout"]
+boto_max_connections = config["boto_max_connections"]
+lambda_name = config["lambda"]["name"]
+lambda_zip = config["lambda"]["zip"]
+
+lambda_config = Config(read_timeout=lambda_read_timeout, max_pool_connections=boto_max_connections)
+lambda_client = boto3.client('lambda', config=lambda_config)
+
+
+def write_to_s3(bucket, key, data):
+    s3.Bucket(bucket).put_object(Key=key, Body=data)
+
+
+def zipLambda(fname, zipname):
+    subprocess.call(['zip', zipname] + glob.glob(fname + '.py'))
+
+
+def removeZip(zipname):
+    subprocess.call(['rm', '-rf', zipname])
+
+
+def invoke_lambda(current_iter, end_iter, remain_page, file, pagerank_init):
+    '''
+    Lambda 함수를 호출(invoke) 합니다.
+    '''
+
+    resp = lambda_client.invoke(
+        FunctionName=lambda_name,
+        InvocationType='RequestResponse',
+        Payload=json.dumps({
+            "current_iter": current_iter,
+            "end_iter": end_iter,
+            "remain_page": remain_page,
+            "file": file,
+            "pagerank_init": pagerank_init
+        })
+    )
+
+
+def get_s3_object(bucket, key):
+    response = s3_client.get_object(Bucket=bucket, Key=key)
+    return json.loads(response['Body'].read().decode())
+
+
+# page들의 관계 데이터셋을 만들어 반환하는 함수 입니다.
+def get_page_relation(file):
+    return get_s3_object(bucket, config['relationPrefix'] + file)
+
+
+zipLambda(lambda_name, lambda_zip)
+l_pagerank = lambdautils.LambdaManager(lambda_client, s3_client, region, config["lambda"]["zip"], lambda_name,
+                                       config["lambda"]["handler"])
+l_pagerank.update_code_or_create_on_noexist()
+
+# page의 관계들이 담겨있는 파일을 가지고 dictionary 관계 데이터셋을 만듭니다.
+# page_relations = {}
+divided_page_num = config["divided_page_num"]
+invoked_lambda_num = config["invoked_lambda_num"]
+
+# 전체 페이지의 개수를 계산합니다.
+# for i in range(invoked_lambda_num + 1):
+#     try:
+#         page_relations.update(get_s3_object(bucket, config['relationPrefix'] + str(i) + '.txt'))
+#     except:
+#         pass
+page_relations = []
+total_pages = get_s3_object(bucket, config['relationPrefix'] + 'total_page.txt')
+
+total_page_length = len(total_pages)
+pagerank_init = 1 / total_page_length
+rank_path = '/mnt/efs/' + 'rank_file'
+
+
+# DynamoDB에 모든 페이지의 초기값들을 업로드 합니다.
+def init_iter(page):
+    with open(rank_path, 'r+b', 0) as f:
+        # file lock : start_byte 부터 10개의 byte 범위를 lock
+        fcntl.lockf(f, fcntl.LOCK_EX, 10, page, 1)
+        f.seek(page * 10)
+        f.write(pagerank_init)
+        # file lock : start_byte 부터 10개의 byte 범위를 unlock
+        fcntl.lockf(f, fcntl.LOCK_UN, page, 1)
+        f.close()
+
+
+init_return = []
+for page in total_pages:
+    init_t = Thread(target=init_iter,
+                    args=(page,))
+    print(page, '번째 페이지 init 시작')
+    init_t.start()
+    init_return.append(init_t)
+for init_t in init_return:
+    init_t.join()
+
+print('init 끝')
+# 모든 page의 초기 Rank값은 1/(전체 페이지 수) 의 값을 가집니다.
+
+# 앞서 zip으로 만든 파일이 Lambda에 업로드 되었으므로 로컬에서의 zip파일을 삭제합니다.
+removeZip(lambda_zip)
+# 반복 횟수를 설정합니다.
+end_iter = 10
+dampen_factor = 0.8
+remain_page = (1 - dampen_factor) / total_page_length
+
+print('pages 총 개수:', total_page_length)
+print('pages 분할 개수:', divided_page_num)
+
+# S3의 나뉘어진 파일 수 만큼 람다를 병렬적으로 Invoke합니다.
+# t_return = []
+# for idx in range(102 + 1):
+#     s3_file_path = config['relationPrefix'] + str(idx) + '.txt'
+#     print(idx, '번째 invoking')
+#     t = Thread(target=invoke_lambda,
+#                args=(1, end_iter, remain_page, s3_file_path, pagerank_init))
+#     t.start()
+#     t_return.append(t)
+# for t in t_return:
+#     t.join()
+
+for idx in range(invoked_lambda_num + 1):
+    try:
+        s3_file_path = config['relationPrefix'] + str(idx) + '.txt'
+        print(idx, '번째 invoking')
+        invoke_lambda(1, end_iter, remain_page, s3_file_path, pagerank_init)
+    except:
+        pass
---
+++ pagerank_lambda/efs/pagerank.py
@@ -0,0 +1,111 @@
+import boto3
+import json
+import resource
+import time
+import decimal
+from botocore.client import Config
+from threading import Thread
+
+# S3 session 생성
+s3 = boto3.resource('s3')
+s3_client = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+db_name = 'jg-pagerank'
+rank_table = dynamodb.Table(db_name)
+
+lambda_read_timeout = 300
+boto_max_connections = 1000
+lambda_config = Config(read_timeout=lambda_read_timeout, max_pool_connections=boto_max_connections)
+lambda_client = boto3.client('lambda', config=lambda_config)
+lambda_name = 'pagerank'
+bucket = "jg-pagerank-bucket"
+
+
+# 주어진 bucket 위치 경로에 파일 이름이 key인 object와 data를 저장합니다.
+def write_to_s3(bucket, key):
+    s3.Bucket(bucket).put_object(Key=key)
+
+
+def get_s3_object(bucket, key):
+    response = s3_client.get_object(Bucket=bucket, Key=key)
+    return json.loads(response['Body'].read().decode())
+
+
+def invoke_lambda(current_iter, end_iter, remain_page, file):
+    '''
+    Lambda 함수를 호출(invoke) 합니다.
+    '''
+
+    resp = lambda_client.invoke(
+        FunctionName=lambda_name,
+        InvocationType='RequestResponse',
+        Payload=json.dumps({
+            "current_iter": current_iter,
+            "end_iter": end_iter,
+            "remain_page": remain_page,
+            "file": file,
+        })
+    )
+
+
+def get_past_pagerank(t, page):
+    past_pagerank = t.get_item(Key={'page': str(page)})
+    return past_pagerank['Item']
+
+
+def put_dynamodb_items(page, iter, rank, relation_length):
+    rank_table.put_item(
+        Item={
+            'iter': iter,
+            'page': str(page),
+            'rank': decimal.Decimal(str(rank)),
+            'relation_length': decimal.Decimal(str(relation_length))
+        }
+    )
+
+
+dampen_factor = 0.8
+
+
+# 랭크를 계산합니다.
+def ranking(page_relation):
+    rank = 0
+    get_time = 0
+    for page in page_relation:
+        # dynamodb에 올려져 있는 해당 페이지의 rank를 가져옵니다.
+        get_start = time.time()
+        past_info = get_past_pagerank(rank_table, page)
+        get_time += time.time() - get_start
+        rank += float(past_info['rank']) / float(past_info['relation_length'])
+    rank *= dampen_factor
+    return rank, get_time
+
+
+# 각각 페이지에 대하여 rank를 계산하고 dynamodb에 업데이트 합니다.
+def ranking_each_page(page, page_relation, iter, remain_page):
+    rank_start = time.time()
+    rank, get_time = ranking(page_relation)
+    page_rank = rank + remain_page
+    rank_time = time.time() - rank_start
+    put_start = time.time()
+    put_dynamodb_items(page, iter, page_rank, len(page_relation))
+    put_time = time.time() - put_start
+    return {'iter': iter, 'page': page, 'get_time': get_time, 'rank_time': rank_time, 'put_time': put_time}
+
+
+def lambda_handler(event, context):
+    current_iter = event['current_iter']
+    end_iter = event['end_iter']
+    remain_page = event['remain_page']
+    file = event['file']
+    page_relations = get_s3_object(bucket, file)
+    try:
+        for page, page_relation in page_relations.items():
+            ranking_result = ranking_each_page(page, page_relation, current_iter, remain_page)
+            print(ranking_result)
+        # current_iter = end_iter이 되기 전 까지 다음 iteration 람다를 invoke합니다.
+        if current_iter < end_iter:
+            invoke_lambda(current_iter + 1, end_iter, remain_page, file)
+    except:
+        pass
+    return True
