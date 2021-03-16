12942498d69fd260911af8993d0f08278e3d79a7
manchann <chl8273@naver.com>
2021-03-17 06:55:45 +0900

    test

--- pagerank_lambda/mysql/driver.py
+++ pagerank_lambda/mysql/driver.py
@@ -105,21 +105,20 @@ pagerank_init = 1 / total_page_length
 conn = pymysql.connect(host=host, user=user_name, port=port,
                        passwd=pwd, db=db_name)
 cur = conn.cursor(pymysql.cursors.DictCursor)
-cur.execute(
-    "CREATE TABLE pagerank (page VARCHAR(255) NOT NULL, iter VARCHAR(255) NOT NULL, `rank` VARCHAR(255), `relation_length` VARCHAR(255))"
-)
+# cur.execute(
+#     "CREATE TABLE pagerank (page VARCHAR(255) NOT NULL, iter VARCHAR(255) NOT NULL, `rank` VARCHAR(255), `relation_length` VARCHAR(255))"
+# )
 # cur.execute('DROP table pagerank')
-cur.execute('use pagerank')
-cur.execute('select * from pagerank')
-# for page in total_pages:
-#     try:
-#         page_relation = page_relations[page]
-#     except:
-#         page_relation = ['-1']
-#     cur.execute('INSERT INTO pagerank (page,iter,rank,relation_length) VALUES(%s,%s,%s,%s)',
-#                 (page, 0, pagerank_init, len(page_relation)))
-#     print(cur.fetchone())
-#     conn.commit()
+
+for page in total_pages:
+    try:
+        page_relation = page_relations[page]
+    except:
+        page_relation = ['-1']
+    cur.execute('INSERT INTO pagerank (page,iter,rank,relation_length) VALUES(%s,%s,%s,%s)',
+                (page, 0, pagerank_init, len(page_relation)))
+    print(cur.fetchone())
+    conn.commit()
 
 print('init 끝')
 # 모든 page의 초기 Rank값은 1/(전체 페이지 수) 의 값을 가집니다.
