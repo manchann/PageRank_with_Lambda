907f1461ac6590944a184e5df1bee98b069e9edc
manchann <chl8273@naver.com>
2021-03-16 18:00:09 +0900

    test

--- pagerank_lambda/mysql/driver.py
+++ pagerank_lambda/mysql/driver.py
@@ -107,7 +107,7 @@ conn = pymysql.connect(host=host, user=user_name, port=port,
 cur = conn.cursor(pymysql.cursors.DictCursor)
 query = "CREATE TABLE `pagerank` (page VARCHAR(255) NOT NULL, iter VARCHAR(255) NOT NULL, rank VARCHAR(255) NOT NULL, `relation_length` VARCHAR(255) NOT NULL)"
 # cur.execute(query)
-
+cur.execute("USE pagerank;")
 for page in total_pages:
     try:
         page_relation = page_relations[page]
