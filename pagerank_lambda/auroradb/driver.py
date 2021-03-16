18b5acc31a8c8b710ba87a6d8f6c2e1eb868291f
manchann <chl8273@naver.com>
2021-03-16 13:29:40 +0900

    format 변경

--- pagerank_lambda/auroradb/driver.py
+++ pagerank_lambda/auroradb/driver.py
@@ -115,7 +115,7 @@ for page in total_pages:
     except:
         page_relation = ['-1']
     cur.execute('INSERT OR REPLACE INTO pagerank (page,iter,rank,relation_length) VALUES(%s,%s,%s,%S)',
-                (page, 0, pagerank_init, len(page_relation)))
+                (page, '0', str(pagerank_init), str(len(page_relation))))
     print(cur.fetchone())
     conn.commit()
 
