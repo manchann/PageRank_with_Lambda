2fdd4b5d03ba55a265e71c40d0498d43c871c23b
manchann <chl8273@naver.com>
2021-03-17 19:34:38 +0900

    rank repair

--- pagerank_lambda/aurora-postgresql/driver.py
+++ pagerank_lambda/aurora-postgresql/driver.py
@@ -107,7 +107,7 @@ conn = psycopg2.connect(host=host, user=user_name, port=port,
 cur = conn.cursor()
 cur.execute('DROP table pagerank')
 cur.execute(
-    "CREATE TABLE pagerank (page VARCHAR(255) NOT NULL, iter VARCHAR(255) NOT NULL, `rank` VARCHAR(255), `relation_length` VARCHAR(255), primary key(page))"
+    "CREATE TABLE pagerank (page VARCHAR(255) NOT NULL, iter VARCHAR(255) NOT NULL, rank VARCHAR(255), relation_length VARCHAR(255), primary key(page))"
 )
 
 for page in total_pages:
