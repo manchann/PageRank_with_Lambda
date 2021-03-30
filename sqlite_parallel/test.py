99c49eddc714b0460e46f3034f5922a77d63191e
manchann <chl8273@naver.com>
2021-03-30 12:02:14 +0900

    test thread 4

--- sqlite_parallel/test.py
+++ sqlite_parallel/test.py
@@ -128,10 +128,10 @@ def lambda_handler(current_iter, end_iter, remain_page, file, idx):
     start = time.time()
     page_relations = get_s3_object(bucket, file)
     print('s3 get 걸린 시간', time.time() - start)
+    conn = sqlite3.connect(db_path, timeout=600, check_same_thread=False)
+    cur = conn.cursor()
     try:
         while current_iter <= end_iter:
-            conn = sqlite3.connect(db_path, timeout=600, check_same_thread=False)
-            cur = conn.cursor()
             cur.execute('pragma journal_mode=wal')
             cur.execute('pragma busy_timeout=600;')
             conn.commit()
