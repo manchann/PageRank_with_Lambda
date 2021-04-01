8e34aa984dd0ac3e7f8d845b9fb19b7890eecb67
manchann <chl8273@naver.com>
2021-04-01 13:11:00 +0900

    mysql create table

--- pagerank_lambda/auroradb-mysql/driver.py
+++ pagerank_lambda/auroradb-mysql/driver.py
@@ -106,9 +106,9 @@ pagerank_init = 1 / total_page_length
 conn = pymysql.connect(host=host, user=user_name, port=port,
                        passwd=pwd, db=db_name)
 cur = conn.cursor(pymysql.cursors.DictCursor)
-# cur.execute(
-#     "CREATE TABLE pagerank (page VARCHAR(255) NOT NULL, iter VARCHAR(255) NOT NULL, rank VARCHAR(255) NOT NULL, relation_length VARCHAR(255) NOT NULL)"
-# )
+cur.execute(
+    "CREATE TABLE pagerank (page VARCHAR(255) NOT NULL, iter VARCHAR(255) NOT NULL, rank VARCHAR(255) NOT NULL, relation_length VARCHAR(255) NOT NULL)"
+)
 
 for page in total_pages:
     try:
--- pagerank_lambda/mysql/driver.py
+++ pagerank_lambda/mysql/driver.py
@@ -106,10 +106,10 @@ pagerank_init = 1 / total_page_length
 conn = pymysql.connect(host=host, user=user_name, port=port,
                        passwd=pwd, db=db_name)
 cur = conn.cursor(pymysql.cursors.DictCursor)
-# cur.execute('DROP table pagerank')
-# cur.execute(
-#     "CREATE TABLE pagerank (page VARCHAR(255) NOT NULL, iter VARCHAR(255) NOT NULL, `rank` VARCHAR(255), `relation_length` VARCHAR(255), primary key(page))"
-# )
+cur.execute('DROP table pagerank')
+cur.execute(
+    "CREATE TABLE pagerank (page VARCHAR(255) NOT NULL, iter VARCHAR(255) NOT NULL, `rank` VARCHAR(255), `relation_length` VARCHAR(255), primary key(page))"
+)
 
 for page in total_pages:
     try:
