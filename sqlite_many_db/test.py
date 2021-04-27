814ae4694d917a8ff2d90e86c35ae208a49bf025
manchann <chl8273@naver.com>
2021-04-27 15:29:15 +0900

    testint about lambda db count

---
+++ sqlite_many_db/test.py
@@ -0,0 +1,38 @@
+import json
+import boto3
+import glob
+import os
+import subprocess
+import decimal
+import time
+from botocore.client import Config
+from boto3.dynamodb.types import DYNAMODB_CONTEXT
+from threading import Thread
+import fcntl
+import sqlite3
+
+db_name = 'test.db'
+db_path = '/mnt/efs/ap/'
+
+
+def write(db):
+    db = db_path + db
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+
+    cur.execute('''CREATE TABLE if not exists test(
+                             name INTEGER NOT NULL PRIMARY KEY,
+                          )''')
+
+    name = str(time.time())
+    cur.execute('INSERT OR REPLACE INTO test VALUES (?)', (name))
+    conn.commit()
+
+
+t_return = []
+for idx in range(2):
+    t = Thread(target=write, args=idx)
+    t.start()
+    t_return.append(t)
+for t in t_return:
+    t.join()
